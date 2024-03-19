f29f9dc2a84fc6dfbda7d3733dcd968bea5470d1
Mikhail Samigullin <mixakun@gmail.com>
2024-03-19 10:04:00 +0400

    Add RestControllerAdvice

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ControllerExceptionHandler.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.util.ApiError;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Object> handleValidationExceptions(
+          MethodArgumentNotValidException exception, HttpServletRequest request) {
+    Map<String, String> errors = new HashMap<>();
+    exception.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    log.info("Получен запрос к эндпоинту:  \"" + request.getRequestURI() + "\", ошибки: " + exception.getFieldErrors());
+    ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST.value(), request.getRequestURI(), HttpStatus.BAD_REQUEST.getReasonPhrase(), errors, LocalDateTime.now());
+    return new ResponseEntity<>(apiError, new HttpHeaders(), apiError.getStatus());
+  }
+
+}
--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,28 +1,18 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.util.ApiError;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.util.Util;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @Slf4j
@@ -44,28 +34,13 @@ public class FilmController {
   }
 
   @PutMapping
-  public Film update(@Valid @RequestBody Film film) throws RuntimeException {
+  public Film update(@Valid @RequestBody Film film) {
     if (films.containsKey(film.getId())) {
       films.put(film.getId(), film);
     } else {
       throw new RuntimeException("Такого id не существует.");
     }
     return film;
-  }
-
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Object> handleValidationExceptions(
-          MethodArgumentNotValidException ex) {
-    Map<String, String> errors = new HashMap<>();
-    ex.getBindingResult().getAllErrors().forEach((error) -> {
-      String fieldName = ((FieldError) error).getField();
-      String errorMessage = error.getDefaultMessage();
-      errors.put(fieldName, errorMessage);
-    });
-    log.info("Получен запрос к эндпоинту:  \"/films\", ошибки: " + ex.getFieldErrors());
-    ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST.value(), "/films", HttpStatus.BAD_REQUEST.getReasonPhrase(), errors, LocalDateTime.now());
-    return new ResponseEntity<>(apiError, new HttpHeaders(), apiError.getStatus());
   }
 
 }
