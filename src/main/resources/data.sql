31497672cf392437e17a995c2cbe92970e4307d8
Mikhail Samigullin <mixakun@gmail.com>
2024-04-29 11:08:42 +0400

    Add database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into AGE_RATING(NAME)
+values ('G'),
+       ('PG'),
+       ('PG-13'),
+       ('R'),
+       ('NC-17');
+
+insert into GENRE(NAME)
+values ('Комедия'),
+       ('Драма'),
+       ('Мультфильм'),
+       ('Триллер'),
+       ('Документальный'),
+       ('Боевик');
